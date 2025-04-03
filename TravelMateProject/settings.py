c2f81f517b696a46454c47398a157750547985e8
richVlady <richvlady@gmail.com>
2025-04-03 19:10:50 -0400

    Added the account functionality, allowing users to make accounts, login, logout, ext. Also added admin selection, and several UI imporvments.

--- TravelMateProject/settings.py
+++ TravelMateProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'trips',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'core:landing'
+LOGOUT_REDIRECT_URL = 'core:landing'
