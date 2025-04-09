02a4848f4195dc2151a4e6d400fe81fa72b00a0b
sampeterson1 <yoshiapolis@gmail.com>
2025-04-08 20:00:29 -0500

    TMATE-22

--- trips/models.py
+++ trips/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Trip(models.Model):
+    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     destination = models.CharField(max_length=200)
+    latitude = models.DecimalField(max_digits=15, decimal_places=10)
+    longitude = models.DecimalField(max_digits=15, decimal_places=10)
     start_date = models.DateField()
     end_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
--- trips/urls.py
+++ trips/urls.py
@@ -5,4 +5,5 @@ app_name = 'trips'
 
 urlpatterns = [
     path('planner/', views.trip_planner, name='trip_planner'),
+    path('<int:id>/weather', views.view_weather, name='view_weather'),
 ] 
